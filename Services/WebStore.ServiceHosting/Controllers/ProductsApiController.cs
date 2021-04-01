fa3de7f6f883e6007edb8c8f4037df1b4d29e5af
Serg <serg@example.com>
2021-04-01 22:22:22 +0300

    Заготовка Api каталога товаров

---
+++ Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase, IProductData
+    {
+        private readonly IProductData _productData;
+
+        public ProductsApiController(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        [HttpGet("brands")]
+        public IEnumerable<Brand> GetBrands()
+        {
+            return _productData.GetBrands();
+        }
+
+        [HttpGet("products/{id}")]
+        public Product GetProductById(int id)
+        {
+            return _productData.GetProductById(id);
+        }
+
+        [HttpPost("products")]
+        public IEnumerable<Product> GetProducts([FromBody]ProductFilter productFilter = null)
+        {
+            return _productData.GetProducts(); 
+        }
+
+        [HttpGet("sections")]
+        public IEnumerable<Section> GetSections()
+        {
+            return _productData.GetSections();
+        }
+    }
+}
