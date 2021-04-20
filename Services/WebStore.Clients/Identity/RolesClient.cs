944a7ec171b00b81d445f8a239fb59f6288fbded
Serg <serg@example.com>
2021-04-20 15:40:12 +0300

    Заготовки клиентов

---
+++ Services/WebStore.Clients/Identity/RolesClient.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WebStore.Clients.Identity
+{
+    class RolesClient : BaseClient
+    {
+        public RolesClient(IConfiguration configuration) : base(configuration, "/api/RolesApi")
+        {
+        }
+    }
+}
